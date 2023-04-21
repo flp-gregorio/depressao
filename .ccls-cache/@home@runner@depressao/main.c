99fcb093df6933ff777559fe1d5a02b410f36a8d
flp-gregorio <113314823+flp-gregorio@users.noreply.github.com>
2023-04-21 19:44:23 +0000

    Commit 1

--- .ccls-cache/@home@runner@depressao/main.c
+++ .ccls-cache/@home@runner@depressao/main.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 int main() {
-    int a[] = {1, 234, 345, 342, 5634, 523, 52, 564, 6, 345, 2, 345, 235};
+    int a[] = {1, 234, 345, 342, 5634, 523, 52, 564, 6, 345, 2, 345, 23};
     int n = sizeof(a) / sizeof(a[0]);
     int comparisons = 0;
     int swaps = 0;
